fbf9b8d55101f9397dec2eb1475bd68c80f02b9a
Abuzer22 <abuzerakbar33@gmail.com>
2025-11-18 00:37:39 +0530

    2nd change

--- Lecture 7(adv pattern (part -2))/abu.java
+++ Lecture 7(adv pattern (part -2))/abu.java
@@ -4,13 +4,6 @@ public class abu {
 
     public static void main(String[] args) {
         System.out.println("hello world !");
-        System.out.println("hello world !");
-        System.out.println("hello world !");
-        System.out.println("hello world !");
-        System.out.println("hello world !");
-        System.out.println("hello world !");
-        System.out.println("hello world !");
-        System.out.println("hello world !");
-        System.out.println("hello world !");
-    }
+        System.out.println(" abuzer");
+        }
 }
