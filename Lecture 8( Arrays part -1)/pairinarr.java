121a2924962ccd406fcabcd4894b49293f14eb3b
Abuzer22 <abuzerakbar33@gmail.com>
2026-01-14 01:45:38 +0530

    adding arr file

---
+++ Lecture 8( Arrays part -1)/pairinarr.java
@@ -0,0 +1,20 @@
+public class pairinarr {
+    public static void printpairs(int numbers[]){
+
+        int tp =0;
+
+        for( int i = 0; i<numbers.length;i++ ){
+            // int curr = numbers[i];
+            for(int j = i+1; j < numbers.length; j++){
+                System.out.print("(" +numbers[i]+ "," +numbers[j]+ ")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs = " +tp);    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        printpairs(numbers);
+    }
+}
---
+++ Lecture 8( Arrays part -1)/reversearr.java
@@ -0,0 +1,25 @@
+public class reversearr {
+    public static void rvrsesarr(int number[]){
+        int first = 0, last = number.length-1;
+
+        while (first < last) {
+            // swap
+            int temp = number[last];
+            number[last] = number[first];
+            number[first]= temp;
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int number[] = {2,4,6,8,10,12,14,16,18};
+        rvrsesarr(number);
+        for( int i =0; i< number.length; i++){
+            System.out.print(number[i]+ " ");
+
+        }
+        System.out.println();
+    }
+}
