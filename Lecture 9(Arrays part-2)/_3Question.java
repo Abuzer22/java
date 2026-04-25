b19d07544dd127a487e3614209469d14289051bb
Abuzer22 <abuzerakbar33@gmail.com>
2026-04-25 09:00:48 +0530

    quesion of array

--- Lecture 9(Arrays part-2)/_3Question.java
+++ Lecture 9(Arrays part-2)/_3Question.java
@@ -4,5 +4,25 @@
 // Explanation:Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5. Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
 public class _3Question {
-    
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int buyprice = Integer.MAX_VALUE;
+        // int buyprice = prices[0];
+        int maxprofit = 0;
+        for(int i = 0; i< n; i++){
+            if( buyprice <= prices[i]){
+                int profit = prices[i] - buyprice;
+                maxprofit = Math.max(maxprofit, profit);
+            }
+            else{
+                buyprice = prices[i];
+            }
+        }
+        return maxprofit;
+    }
+
+    public static void main(String[] args) {
+        int prices [] =  {7, 1, 5, 3, 6,  4};
+        System.out.println(maxProfit(prices));
+    }
 }
